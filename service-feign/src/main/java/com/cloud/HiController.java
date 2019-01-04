aea79b3b92483cde526d58c7556ff4ce2a5dd2ef
ghzq <ghzq@ghzqdeMacBook-Pro.local>
2019-01-04 09:22:41 +0800

    first commit

---
+++ service-feign/src/main/java/com/cloud/HiController.java
@@ -0,0 +1,20 @@
+package com.cloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class HiController {
+
+    @Resource
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name){
+        return  schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
---
+++ src/main/java/com/cloud/SpringCloudExamplesApplication.java
@@ -0,0 +1,14 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudExamplesApplication.class, args);
+    }
+
+}
+
